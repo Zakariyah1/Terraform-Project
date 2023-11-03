9104948d74599a9d8d768a5de56d12adff5eb041
Zakariyah1 <zakariyahdaher1@gmail.com>
2023-11-03 16:17:17 +0000

    first commit

---
+++ Terraform/modules/vpc/variables.tf
@@ -0,0 +1,23 @@
+variable "cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+   default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "The CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "The CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "availability_zone" {
+  description = "The availability zone to create resources in"
+  type        = string
+  default     = "eu-west-2a"
+}
